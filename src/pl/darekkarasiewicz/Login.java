59c2ea30b0587ae81ad2e4c81810ef8f39453a44
DarekKarasiewicz <dar.karasiewicz@gmail.com>
2021-06-09 15:09:16 +0200

    Add funkcion to login to check bans

--- src/pl/darekkarasiewicz/Login.java
+++ src/pl/darekkarasiewicz/Login.java
@@ -4,33 +4,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Scanner;
 
-public class Login  {
-    private int a=4;
+public class Login {
+    private int a = 4;
     private Account[] konta;
-    private JFrame window1 =new JFrame("Login");
-    private JTextField nr=new JTextField();
-    private JPasswordField password= new JPasswordField();
+    private JFrame window1 = new JFrame("Login");
+    private JTextField nr = new JTextField();
+    private JPasswordField password = new JPasswordField();
     private JLabel labelPass = new JLabel();
     private JLabel labelnr = new JLabel();
     private JLabel labelerror = new JLabel();
-    private JButton loginButton= new JButton("Login");
+    private JButton loginButton = new JButton("Login");
+
     public Login(Account[] konta) {
-        this.konta=konta;
-        labelnr.setBounds(15,30,80,25);
-        nr.setBounds(100,30,165,25);
-        password.setBounds(100,60,165,25);
-        labelPass.setBounds(15,60,80,25);
+        this.konta = konta;
+        labelnr.setBounds(15, 30, 80, 25);
+        nr.setBounds(100, 30, 165, 25);
+        password.setBounds(100, 60, 165, 25);
+        labelPass.setBounds(15, 60, 80, 25);
         labelPass.setText("Pin");
         labelnr.setText("Nr.");
-        labelerror.setBounds(125,75,70,50);
+        labelerror.setBounds(125, 75, 70, 50);
         labelerror.setText(String.format("Error"));
         labelerror.getBorder();
         labelerror.setBackground(Color.red);
         labelerror.setVisible(false);
-        loginButton.setBounds(105,125,70,30);
+        loginButton.setBounds(105, 125, 70, 30);
         window1.setLayout(null);
-        window1.setSize(300,200);
+        window1.setSize(300, 200);
         window1.setLocationRelativeTo(null);
         window1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window1.add(labelerror);
@@ -41,36 +47,47 @@ public class Login  {
         window1.add(loginButton);
         window1.setResizable(false);
 
+
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-
-
-                if (a>=-1){
-                for (Account account : konta) {
-                    if (account.getNumber()==Integer.parseInt(nr.getText())){
-                        Account account1=account;
-                        if (Integer.parseInt(String.valueOf(password.getPassword()))==account1.getPin()) {
-                            window1.dispose();
-                            MainWindow mainWindow= new MainWindow(account);
+                    HashSet<String> bans = new HashSet<>();
+                    File file = new File("C:\\Users\\darek\\IdeaProjects\\Projekt\\Ban.txt");
+                    Scanner scanner = new Scanner(file);
+                    while (scanner.hasNextLine()) {
+                        bans.add(scanner.nextLine());
                     }
-                        else if (Integer.parseInt(String.valueOf(password.getPassword()))!=account1.getPin()){
-                            labelerror.setVisible(true);
-                            a-=1;
+                    for (String ban : bans) {
+                        if (Integer.parseInt(ban) == Integer.parseInt(String.valueOf(password.getPassword()))){
+                        JOptionPane.showMessageDialog(null,"Zbanowany","error",JOptionPane.ERROR_MESSAGE);
+                        window1.dispose();
+                        a=2;
+                        }
+
                     }
 
-                }
-                }
+                    if (a >= 3) {
+                            for (Account account : konta) {
+                                if (account.getNumber() == Integer.parseInt(nr.getText())) {
+                                    Account account1 = account;
+                                    if (Integer.parseInt(String.valueOf(password.getPassword())) == account1.getPin()) {
+                                        window1.dispose();
+                                        MainWindow mainWindow = new MainWindow(account);
+                                    } else if (Integer.parseInt(String.valueOf(password.getPassword())) != account1.getPin()) {
+                                        labelerror.setVisible(true);
+                                        a -= 1;
+                                    }
 
-
+                                }
+                            }
+                    } else {
+                        window1.dispose();
+                    }
+                } catch (NumberFormatException | FileNotFoundException a) {
+                    JOptionPane.showMessageDialog(null, "Nr lub pin muszą być liczbą", "error", JOptionPane.ERROR_MESSAGE);
                 }
-            else {
-                window1.dispose();
-                }}
-               catch (NumberFormatException a){
-                   JOptionPane.showMessageDialog(null,"Nr lub pin muszą być liczbą","error",JOptionPane.ERROR_MESSAGE);
-            }}
+            }
         });
         window1.setVisible(true);
     }
--- src/pl/darekkarasiewicz/Main.java
+++ src/pl/darekkarasiewicz/Main.java
@@ -10,7 +10,10 @@ public class Main {
         Account nr1= new Account(1111,1,2000);
         Account nr2= new Account(2222,2,3000);
         Account nr3= new Account(3333,3,5000);
-        Account [] konta = new Account[]{nr1,nr2,nr3};
+        Account nr4= new Account(4444,4,2000);
+        Account nr5= new Account(5555,5,2000);
+        Account nr6= new Account(6666,6,2000);
+        Account [] konta = new Account[]{nr1,nr2,nr3,nr4,nr5,nr6};
         Login login= new Login(konta);
         //MainWindow mainWindow =new MainWindow(1);
     }
